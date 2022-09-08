1f0ad25fc715118cdc6174cc5107876e85dcea73
PrtPee <peerapat1202@gmail.com>
2022-09-08 12:04:27 +0700

    Create Exercise4_Peerapat_R.py

---
+++ Exercise4_Peerapat_R.py
@@ -0,0 +1,10 @@
+FE = 79.5
+GB = 77.0
+CS = 89.5
+CP = 91.9
+
+print("---Your Score---")
+print("Foundation English                 :     ",FE)
+print("General Business                   :     ",GB)
+print("Introduction to Computer Systems   :     ",CS)
+print("Computer Programming               :     ",CP)
